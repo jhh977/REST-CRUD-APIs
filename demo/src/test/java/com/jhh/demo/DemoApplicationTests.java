6c1996df821b37ab08f9569721d64971382a9924
unknown <jihadhijazi977@gmail.com>
2025-03-18 16:26:21 +0200

    added essential spring files

---
+++ demo/src/test/java/com/jhh/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhh.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
