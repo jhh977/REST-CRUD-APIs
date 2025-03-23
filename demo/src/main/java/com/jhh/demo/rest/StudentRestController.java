7e30c6e3e7f5bbdbd4b5de8f9850349054a8bb2e
unknown <jihadhijazi977@gmail.com>
2025-03-23 21:22:41 +0200

    added out of bound exception handling

--- demo/src/main/java/com/jhh/demo/rest/StudentRestController.java
+++ demo/src/main/java/com/jhh/demo/rest/StudentRestController.java
@@ -2,10 +2,9 @@ package com.jhh.demo.rest;
 
 import com.jhh.demo.entity.Student;
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,6 +33,27 @@ public class StudentRestController {
     @GetMapping("/students/{studentId}")
     public Student getStudent(@PathVariable int studentId)
     {
+
+//        check the studentID against the list size
+
+        if(studentId > theStudent.size() || studentId < 0)
+        {
+            throw new StudentNotFoundException("Student id not found - " + studentId);
+        }
         return theStudent.get(studentId);
+    }
+
+//    add an exception handler using @ExceptionHandler
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc){
+//        create studentErrorResponse
+        StudentErrorResponse error = new StudentErrorResponse();
+
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+//        return ResponseEntity
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 }
